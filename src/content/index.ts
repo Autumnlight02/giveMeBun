ba63ceacd04b7caf5cc26fecff7119f68174c311
Aykut Akgün <aykut.akguen@it-suisse.com>
2024-04-21 14:35:32 +0200

    gimmeBun

--- src/content/index.ts
+++ src/content/index.ts
@@ -3,7 +3,6 @@ import "./styles.css";
 
 const loc = new URL(document.location.href);
 const locString = loc.host.replace("www.", "").split(".")[0];
-console.log(locString);
 
 switch (locString) {
   case "npmjs": {
